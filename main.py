13d3185ec1a624d49cd687a91cf83e889125e892
Abdullahi Mohammed <abdumok19@gmail.com>
2025-01-14 15:21:20 +0300

    make display board function

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import os
 import time
 
+p = [" "] * 9
+
 # Create function to clear the screen
 def clear():
     os.system("cls" if os.name == "nt" else "clear")
@@ -8,4 +10,23 @@ def clear():
 def welcome():
     print("Welcome to tic toc game:")
     input("Press enter to start the game...")
+
+# Create function to display board status
+def display_board(game_board):
+    
+    board = print(f"""
+|----------|---------|----------|
+|          |         |          |
+|    {p[6]}     |    {p[7]}    |    {p[8]}     |
+|          |         |          |
+|----------|---------|----------|
+|          |         |          |
+|    {p[3]}     |    {p[4]}    |    {p[5]}     |
+|          |         |          |
+|----------|---------|----------|
+|          |         |          |
+|    {p[0]}     |    {p[1]}    |    {p[2]}     |
+|          |         |          |
+|----------|---------|----------|
+""")
 
