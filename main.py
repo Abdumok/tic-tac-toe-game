0c8cf83a76b3cf74572205674174024e6584789d
Abdullahi Mohammed <abdumok19@gmail.com>
2025-01-15 13:39:59 +0300

    add logo, some line to determine who start the game  and some change in variable and function names

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import os
-import time
+import random
+import ascii
 
 
 # Create function to clear the screen
@@ -8,8 +9,9 @@ def clear():
 
 
 def welcome():
+    print(ascii.logo)
     print("Welcome to tic toc game:")
-    input("Press enter to start the game...")
+    input("Press enter to start...\n")
 
 
 # Create function to display board status
@@ -32,11 +34,10 @@ def display_board(board):
 """)
     
 
-
 # Create function to set player symbol:
-def player_symbol():
+def players_symbol():
     while True:
-        player_choice = input("choose a your symbol: (X or O) ").upper()
+        player_choice = input("Plyer1 choose a your symbol: (X or O) ").upper()
         if player_choice == "O":
             player1 = player_choice
             player2 = "X"
@@ -50,15 +51,15 @@ def player_symbol():
     return player1, player2
 
 
-def position_of_play(positions, player_symbol, player_name):
+def position_of_play(board, player_symbol, player_name):
     while True:
         choice = input(f"{player_name} choose a number 1 - 9: ")
         if choice.isdigit():
             choice_int= int(choice)
-            if 0 < choice_int < 10 and positions[choice_int-1] == "-":
-                positions[choice_int-1] = player_symbol
+            if 0 < choice_int < 10 and board[choice_int-1] == "-":
+                board[choice_int-1] = player_symbol
                 break
-            elif positions[choice_int-1] != "-":
+            elif board[choice_int-1] != "-":
                 print("this position is already taken, Try again")
             elif 0 > choice_int > 10:
                 print("Invalid input, Enter number between 1 - 9")
@@ -66,56 +67,80 @@ def position_of_play(positions, player_symbol, player_name):
             print("That is not a number please enter number between (1 - 9)")
 
 
-
-def game_over(p, player):
-    if ((p[0] == p[1] == p[2] == player["symbol"]) or
-        (p[3] == p[4] == p[5] == player["symbol"]) or
-        (p[6] == p[7] == p[8] == player["symbol"]) or
-        (p[0] == p[3] == p[6] == player["symbol"]) or
-        (p[1] == p[4] == p[7] == player["symbol"]) or
-        (p[2] == p[5] == p[8] == player["symbol"]) or
-        (p[0] == p[4] == p[8] == player["symbol"]) or
-        (p[2] == p[4] == p[6] == player["symbol"])):
+# Create function to determine game result
+def game_over(board, player):
+    if ((board[0] == board[1] == board[2] == player["symbol"]) or
+        (board[3] == board[4] == board[5] == player["symbol"]) or
+        (board[6] == board[7] == board[8] == player["symbol"]) or
+        (board[0] == board[3] == board[6] == player["symbol"]) or
+        (board[1] == board[4] == board[7] == player["symbol"]) or
+        (board[2] == board[5] == board[8] == player["symbol"]) or
+        (board[0] == board[4] == board[8] == player["symbol"]) or
+        (board[2] == board[4] == board[6] == player["symbol"])):
         
-        print(f"{player["name"]} is win")
+        clear()
+        display_board(board)
+        
+        print(f"\n{"-" * (len(player["name"])+5)}")
+        print(f"{player["name"]} Wins")
+        print(f"{"-" * (len(player["name"])+5)}\n")
         return True
         
-    elif not "-" in p:
+    elif not "-" in board:
+        clear()
+        display_board(board)
         print("it's a draw")
         return True
 
         
-     
 def game_start():
     
-    empty_board = ["-"] * 9
-    player1_name =  input("Player1, Enter a name: ")
-    player2_name =  input("Player2, Enter a name: ")
+    welcome()    
+    
+    game_board = ["-"] * 9
+    player1_name =  input("Player1: Enter a name: ")
+    player2_name =  input("Player2: Enter a name: ")
 
-    player= player_symbol() 
+    player_symbol= players_symbol() 
 
-    player_info= {"player1" : {"name" : player1_name, "symbol" : player[0]},
-          "player2" : {"name" : player2_name, "symbol" : player[1]}
+    player_info= {
+                  "player1" : {"name": player1_name,
+                             "symbol": player_symbol[0]},
+                  
+                  "player2" : {"name": player2_name,
+                             "symbol": player_symbol[1]}
           }
 
-
-    game_on = True
-
-    while game_on:
     
+    game_on = True
+    
+    random_num = random.randint(1,2)
+    
+    clear()
+    print(f"{player_info[f"player{random_num}"]["name"]} start")
+    input("Press enter to start the game when you are ready...")
+
+    if random_num == 1:
+        player1 = player_info["player1"]
+        player2 = player_info["player2"]
+    else:
+        player1 = player_info["player2"]    
+        player2 = player_info["player1"]
+    
+    while game_on:
         clear()
-        display_board(empty_board)
-        position_of_play(empty_board, player_info["player1"]["symbol"], player_info["player1"]["name"])
-        if game_over(empty_board, player_info["player1"]):
+        display_board(game_board)
+        position_of_play(game_board, player1["symbol"],player1["name"])
+        if game_over(game_board, player1):
             game_on = False
             break
         else:
             pass
         
         clear()
-        display_board(empty_board)
-        position_of_play(empty_board, player_info["player2"]["symbol"], player_info["player2"]["name"])
-        if game_over(empty_board, player_info["player1"]):
+        display_board(game_board)
+        position_of_play(game_board, player2["symbol"], player2["name"])
+        if game_over(game_board, player2):
             game_on = False
             break
         else:
@@ -127,7 +152,8 @@ def game_start():
         game_start()
     else:
         pass
-    
+
+
 game_start()
 
     
